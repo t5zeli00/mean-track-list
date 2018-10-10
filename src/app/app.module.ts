62b7984bf34f9e2b13e82d79c8ad76c0679018d7
Любовь Зеленая <t5zeli00@students.oamk.fi>
2018-10-10 21:28:47 +0300

    test checkbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
@@ -21,6 +22,7 @@ import { TrackListComponent } from './tracks/track-list/track-list.component';
     HttpModule,
     ReactiveFormsModule,
     MatCheckboxModule,
+    MatFormFieldModule,
     BrowserAnimationsModule
   ],
   providers: [],
