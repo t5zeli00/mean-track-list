51aa16426aae8592596711c766226feb449f7f66
Любовь Зеленая <t5zeli00@students.oamk.fi>
2018-10-11 15:32:52 +0300

    test

--- src/app/tracks/track.service.spec.ts
+++ src/app/tracks/track.service.spec.ts
@@ -1,12 +1,15 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject  } from '@angular/core/testing';
 
 import { TrackService } from './track.service';
 
 describe('TrackService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
-
-  it('should be created', () => {
-    const service: TrackService = TestBed.get(TrackService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [TrackService]
+    });
   });
+
+  it('should ...', inject([TrackService], (service: TrackService) => {
+    expect(service).toBeTruthy();
+  }));
 });
